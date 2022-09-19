62d257752f10d719c6c435325d72185c51039b72
Allan <github email address>
2022-09-19 15:32:57 +0300

    build

---
+++ build/precache-manifest.dc5262714c0f55e29e60061b049333b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "41be2dd45a520d50c163",
+    "url": "/static/css/main.171d65b3.chunk.css"
+  },
+  {
+    "revision": "41be2dd45a520d50c163",
+    "url": "/static/js/main.ffdf43c8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7bc70aad8ef99ee974f7",
+    "url": "/static/js/2.348f2e10.chunk.js"
+  },
+  {
+    "revision": "1fdcadecb1a82e89e6d2547bc027976e",
+    "url": "/static/media/iranyekanwebregular.e31c09af.1fdcadec.woff2"
+  },
+  {
+    "revision": "f1594ce19b7c17835378ae9e2dfbd3f6",
+    "url": "/index.html"
+  }
+];
